6894163ec69f348255a4f3c6713437851d237e36
Alexander <145133700+bronzit@users.noreply.github.com>
2025-07-29 12:09:29 +0300

    Update hooks.cpp

--- TempleWare-CS2/source/templeware/hooks/hooks.cpp
+++ TempleWare-CS2/source/templeware/hooks/hooks.cpp
@@ -116,23 +116,25 @@ void* __fastcall H::hkLevelInit(void* pClientModeShared, const char* szNewMap) {
 
 void H::Hooks::init() {
     oGetWeaponData = *reinterpret_cast<int*>(M::patternScan("client", ("48 8B 81 ? ? ? ? 85 D2 78 ? 48 83 FA ? 73 ? F3 0F 10 84 90 ? ? ? ? C3 F3 0F 10 80 ? ? ? ? C3 CC CC CC CC")) + 0x3);
-    ogGetBaseEntity = reinterpret_cast<decltype(ogGetBaseEntity)>(M::patternScan("client", ("81 FA ? ? ? ? 77 ? 8B C2 C1 F8 ? 83 F8 ? 77 ? 48 98 48 8B 4C C1 ? 48 85 C9 74 ? 8B C2 25 ? ? ? ? 48 6B C0 ? 48 03 C8 74 ? 8B 41 ? 25 ? ? ? ? 3B C2 75 ? 48 8B 01")));
-    oGetLocalPlayer = reinterpret_cast<decltype(oGetLocalPlayer)>(M::patternScan("client", ("48 83 EC 28 83 F9 FF 75 17 48 8B 0D ?? ?? ?? ?? 48 8D 54 24 30 48 8B 01 FF 90 ?? ?? ?? ?? 8B 08 48 63 C1 48 8D 0D ?? ?? ?? ?? 48 8B 0C")));
+    // ogGetBaseEntity = reinterpret_cast<decltype(ogGetBaseEntity)>(M::patternScan("client", ("81 FA ? ? ? ? 77 ? 8B C2 C1 F8 ? 83 F8 ? 77 ? 48 98 48 8B 4C C1 ? 48 85 C9 74 ? 8B C2 25 ? ? ? ? 48 6B C0 ? 48 03 C8 74 ? 8B 41 ? 25 ? ? ? ? 3B C2 75 ? 48 8B 01")));
+    oGetLocalPlayer = reinterpret_cast<decltype(oGetLocalPlayer)>(M::patternScan("client", ("48 83 EC 28 83 F9 FF ? ? 48 8B 0D A0 C8 6F 01 48 8D 54 24 30 48 8B 01 ? ? ? ? ? ? 8B 08 48 63 C1 4C 8D 05 A6 09 6F 01 33")));
     
-    UpdateWallsObject.Add((void*)M::patternScan("scenesystem", ("48 89 5C 24 10 48 89 6C 24 18 56 57 41 54 41 56 41 57 48 83 EC 40")), &hkUpdateSceneObject);
-    FrameStageNotify.Add((void*)M::patternScan("client", ("48 89 5C 24 ? 56 48 83 EC 30 8B 05 ? ? ? ?")), &hkFrameStageNotify);
-    DrawArray.Add((void*)M::patternScan("scenesystem", ("48 8B C4 48 89 50 10 53 41 55 41 56 48 81 EC ? ? ? ? 4D 63 F1")), &chams::hook);
+    // UpdateWallsObject.Add((void*)M::patternScan("scenesystem", ("48 89 5C 24 10 48 89 6C 24 18 56 57 41 54 41 56 41 57 48 83 EC 40")), &hkUpdateSceneObject);
+    // FrameStageNotify.Add((void*)M::patternScan("client", ("48 89 5C 24 ? 56 48 83 EC 30 8B 05 ? ? ? ?")), &hkFrameStageNotify);
+    // DrawArray.Add((void*)M::patternScan("scenesystem", ("48 8B C4 48 89 50 10 53 41 55 41 56 48 81 EC ? ? ? ? 4D 63 F1")), &chams::hook);
     GetRenderFov.Add((void*)M::getAbsoluteAddress(M::patternScan("client", "E8 ? ? ? ? F3 0F 11 45 00 48 8B 5C 24 40"), 1), &hkGetRenderFov);
     LevelInit.Add((void*)M::getAbsoluteAddress(M::patternScan("client", "E8 ? ? ? ? C6 83 ? ? ? ? ? C6 83"), 1), &hkLevelInit);
     RenderFlashBangOverlay.Add((void*)M::patternScan("client", ("85 D2 0F 88 ? ? ? ? 55 56 41 55")), &hkRenderFlashbangOverlay);
-    DrawSmokeArray.Add((void*)M::patternScan("client", ("48 89 54 24 ? 55 41 55 48 8D AC 24")), &hkDrawSmokeArray);
+    // DrawSmokeArray.Add((void*)M::patternScan("client", ("48 89 54 24 ? 55 41 55 48 8D AC 24")), &hkDrawSmokeArray);
     DrawScopeOverlay.Add((void*)M::patternScan("client", ("4C 8B DC 53 56 57 48 83 EC")), &hkDrawScopeOverlay);
-    DrawLegs.Add((void*)M::patternScan("client", ("48 89 5C 24 ? 48 89 74 24 ? 48 89 7C 24 ? 55 41 54 41 55 41 56 41 57 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 01")), &hkDrawLegs);
+    // DrawLegs.Add((void*)M::patternScan("client", ("48 89 5C 24 ? 48 89 74 24 ? 48 89 7C 24 ? 55 41 54 41 55 41 56 41 57 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 01")), &hkDrawLegs);
     DrawViewModel.Add((void*)M::patternScan("client", ("48 89 5C 24 ? 48 89 74 24 ? 55 57 41 54 41 56 41 57 48 8B EC 48 83 EC ? 4D 8B E0")), &hkDrawViewModel);
     LightingModulate.Add((void*)M::patternScan("scenesystem", ("48 89 54 24 ? 53 41 56 41 57")), &hkLightingModulate);
     MouseInputEnabled.Add((void*)M::patternScan("client", ("40 53 48 83 EC 20 80 B9 ? ? ? ? ? 48 8B D9 75 78")), &hkMouseInputEnabled);
     IsRelativeMouseMode.Add((void*)M::getvfunc(I::InputSys, 76U), &hkIsRelativeMouseMode);
-    UpdateSkybox.Add((void*)M::patternScan("client", ("48 8B C4 48 89 58 18 48 89 70 20 55 57 41 54 41 55")), &hkUpdateSkybox);
+    // UpdateSkybox.Add((void*)M::patternScan("client", ("48 8B C4 48 89 58 18 48 89 70 20 55 57 41 54 41 55")), &hkUpdateSkybox);
 
     MH_EnableHook(MH_ALL_HOOKS);
 }   
+// Ghidra: ' GetViewAngles ' @client.dll -> "4C 8B C1 85 D2 ? ? 48 8D 05 8A B8 F1 00 ? 8B 81 68 0D 00 00 85 C0"
+// Ghidra: ' SetViewAngles ' @client.dll -> "85 D2 ? ? 48 63 81 68 0D 00 00 F2 41 0F 10 00 85 C0 ? ? 48 69 D0 40 05 00 00 48 03 91 70 0D"
