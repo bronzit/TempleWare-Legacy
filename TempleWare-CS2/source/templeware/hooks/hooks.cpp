515c4c82d6d1d3e7302ec07295eb2c1f1927a4b8
Alexander <145133700+bronzit@users.noreply.github.com>
2025-07-30 10:45:30 +0300

    Update hooks.cpp

--- TempleWare-CS2/source/templeware/hooks/hooks.cpp
+++ TempleWare-CS2/source/templeware/hooks/hooks.cpp
@@ -116,8 +116,8 @@ void* __fastcall H::hkLevelInit(void* pClientModeShared, const char* szNewMap) {
 
 void H::Hooks::init() {
     oGetWeaponData = *reinterpret_cast<int*>(M::patternScan("client", ("48 8B 81 ? ? ? ? 85 D2 78 ? 48 83 FA ? 73 ? F3 0F 10 84 90 ? ? ? ? C3 F3 0F 10 80 ? ? ? ? C3 CC CC CC CC")) + 0x3);
-    // ogGetBaseEntity = reinterpret_cast<decltype(ogGetBaseEntity)>(M::patternScan("client", ("81 FA ? ? ? ? 77 ? 8B C2 C1 F8 ? 83 F8 ? 77 ? 48 98 48 8B 4C C1 ? 48 85 C9 74 ? 8B C2 25 ? ? ? ? 48 6B C0 ? 48 03 C8 74 ? 8B 41 ? 25 ? ? ? ? 3B C2 75 ? 48 8B 01")));
-    // oGetLocalPlayer = reinterpret_cast<decltype(oGetLocalPlayer)>(M::patternScan("client", ("48 83 EC 28 83 F9 FF ? ? 48 8B 0D A0 C8 6F 01 48 8D 54 24 30 48 8B 01 ? ? ? ? ? ? 8B 08 48 63 C1 4C 8D 05 A6 09 6F 01 33")));
+    ogGetBaseEntity = reinterpret_cast<decltype(ogGetBaseEntity)>(M::patternScan("client", ("4C 8D 49 10 81 FA FE 7F 00 00 ? ? 8B CA C1 F9 09 83 F9 3F ? ? 48 63 C1 4D")));
+    oGetLocalPlayer = reinterpret_cast<decltype(oGetLocalPlayer)>(M::patternScan("client", ("48 83 EC 28 83 F9 FF ? ? 48 8B 0D 90 CB 6F 01 48 8D 54 24 30 48 8B 01 ? ? ? ? ? ? 8B 08 48 63 C1 4C 8D 05 86 0C 6F 01 33")));
     
     // UpdateWallsObject.Add((void*)M::patternScan("scenesystem", ("48 89 5C 24 10 48 89 6C 24 18 56 57 41 54 41 56 41 57 48 83 EC 40")), &hkUpdateSceneObject);
     // FrameStageNotify.Add((void*)M::patternScan("client", ("48 89 5C 24 ? 56 48 83 EC 30 8B 05 ? ? ? ?")), &hkFrameStageNotify);
