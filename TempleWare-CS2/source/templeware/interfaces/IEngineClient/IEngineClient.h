4c677aa3f1cd24be99b504d020db7b8f736dd557
Alexander <145133700+bronzit@users.noreply.github.com>
2025-09-07 22:27:09 +0300

    Update IEngineClient.h

--- TempleWare-CS2/source/templeware/interfaces/IEngineClient/IEngineClient.h
+++ TempleWare-CS2/source/templeware/interfaces/IEngineClient/IEngineClient.h
@@ -8,27 +8,27 @@ class IEngineClient
 {
 public:
 	void send_cmd(const char* szCmd) {
-		return M::vfunc<void, 45>(this, 0, szCmd, 0x7FFEF001);
+		return M::vfunc<void, 48>(this, 0, szCmd, 0x7FFEF001);
 	}
 
 	int maxClients()
 	{
-		return M::vfunc<int, 34U>(this);
+		return M::vfunc<int, 36U>(this);
 	}
 
 	bool in_game()
 	{
-		return M::vfunc<bool, 35U>(this);
+		return M::vfunc<bool, 37U>(this);
 	}
 
 	bool connected()
 	{
-		return M::vfunc<bool, 36U>(this);
+		return M::vfunc<bool, 38U>(this);
 	}
 
 	int get_local_player() {
 		int nIndex = -1;
-		M::vfunc<void, 49U>(this, std::ref(nIndex), 0);
+		M::vfunc<void, 52U>(this, std::ref(nIndex), 0);
 		return nIndex + 1;
 	}
 public:
